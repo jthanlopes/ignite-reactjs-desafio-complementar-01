8f2092b25623c143fcedd3ac9726c4ecac952e25
Jonathan Santos <jonathanlpsantos@outlook.com>
2021-10-30 15:33:47 -0300

    Finalização do desafio, componetização da aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface propsContent {
+  selectedGenreTitle: string;
+  movies: Array<MovieProps>;
+}
+
+export function Content({selectedGenreTitle, movies}: propsContent) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
